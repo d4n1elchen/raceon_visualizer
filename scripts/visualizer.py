b76f54f690fa80bde7a9c75c9fcf0fd7a037afde
Daniel Chen <cchen086@usc.edu>
2020-02-16 03:07:02 -0800

    Fix low fps and show fps

--- scripts/visualizer.py
+++ scripts/visualizer.py
@@ -8,6 +8,7 @@ from geometry_msgs.msg import Pose
 from raceon.msg import TrackPosition
 
 # Dependencies for visualization
+import time
 import cv2
 import numpy as np
 
@@ -29,37 +30,59 @@ class Visualizer():
         self.track_width = rospy.get_param(self.param_name_track_width, 600)
         self.camera_center = rospy.get_param(self.param_name_camera_center, 320)
 
+        self.img = np.zeros(shape=[480, 640, 4], dtype=np.uint8)
+        self.width = 640
+        self.height = 480
         self.pos = self.camera_center
         self.left = self.camera_center-self.track_width//2
         self.right = self.camera_center+self.track_width//2
 
-        self.line_width_params = 2
+        self.line_width_params = 4
         self.line_width_indicator = 2
 
-    def start(self):
-        blank_image = np.zeros(shape=[480, 640, 1], dtype=np.uint8)
-        cv2.imshow("Visualizer", blank_image)
+        self.frame_cnt = 0
+        self.time = 0
+        self.fps = 0
 
+    def start(self):
         self.sub_camera = rospy.Subscriber(self.topic_name_camera_image, Image, self.image_callback)
         self.sub_pos_err = rospy.Subscriber(self.topic_name_pos_err, Pose, self.pos_err_callback)
         self.sub_pos_track = rospy.Subscriber(self.topic_name_pos_track, TrackPosition, self.pos_track_callback)
-        rospy.spin()
+        #rospy.spin()
+        self.terminate = False
+        while not self.terminate:
+            self.plot()
 
     def image_callback(self, img_msg):
-        width = img_msg.width
-        height = img_msg.height
+        self.width = img_msg.width
+        self.height = img_msg.height
 
         np_arr = np.frombuffer(img_msg.data, dtype=np.uint8)
-        gray = np_arr.reshape((height, width, 1))
+        gray = np_arr.reshape((self.height, self.width, 1))
 
-        rospy.loginfo("Image with shape {:s} received. (max, min)=({:d}, {:d})".format(str(gray.shape), gray.min(), gray.max()))
+        #rospy.loginfo("Image with shape {:s} received. (max, min)=({:d}, {:d})".format(str(gray.shape), gray.min(), gray.max()))
 
-        img = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+        self.img = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGBA)
+
+
+    def pos_err_callback(self, err_msg):
+        self.pos = int(err_msg.position.x)
+        #rospy.loginfo("Get position info: {:d}".format(self.pos))
+
+    def pos_track_callback(self, track_msg):
+        self.left = int(track_msg.left)
+        self.right = int(track_msg.right)
+        #rospy.loginfo("Get track info: {:d}, {:d}".format(self.left, self.right))
+
+    def plot(self):
+        img = self.img.copy()
+        width = self.width
+        height = self.height
 
         cv2.line(img, (0, self.scan_line), (width, self.scan_line), (255,255,255), self.line_width_params)
-        cv2.line(img, (self.camera_center, height-50), (self.camera_center, height), (0,255,255), self.line_width_params)
-        cv2.line(img, (self.camera_center-self.track_width//2, height-50), (self.camera_center-self.track_width//2, height), (0,0,255), self.line_width_params)
-        cv2.line(img, (self.camera_center+self.track_width//2, height-50), (self.camera_center+self.track_width//2, height), (0,0,255), self.line_width_params)
+        cv2.line(img, (self.camera_center, self.scan_line-20), (self.camera_center, self.scan_line+20), (0,255,255), self.line_width_params)
+        cv2.line(img, (self.camera_center-self.track_width//2, self.scan_line-20), (self.camera_center-self.track_width//2, self.scan_line+20), (0,0,255,0.4), self.line_width_params)
+        cv2.line(img, (self.camera_center+self.track_width//2, self.scan_line-20), (self.camera_center+self.track_width//2, self.scan_line+20), (0,0,255,0.4), self.line_width_params)
 
         cv2.line(img, (self.camera_center-self.pos, 0), (self.camera_center-self.pos, height), (255,0,255), self.line_width_indicator)
         if(self.left):
@@ -67,17 +90,18 @@ class Visualizer():
         if(self.right):
             cv2.line(img, (self.right, 0), (self.right, height), (0,255,0), self.line_width_indicator)
 
+        if self.frame_cnt == 0:
+            self.time = time.time()
+        self.frame_cnt = (self.frame_cnt + 1) % 10
+        if self.frame_cnt == 0:
+            self.fps = 10 / (time.time() - self.time)
+        cv2.putText(img, f"{self.fps:.3f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,0), 2)
+
         cv2.imshow("Visualizer", img)
-        cv2.waitKey(1)
+        key = cv2.waitKey(33)
 
-    def pos_err_callback(self, err_msg):
-        self.pos = int(err_msg.position.x)
-        rospy.loginfo("Get position info: {:d}".format(self.pos))
-
-    def pos_track_callback(self, track_msg):
-        self.left = int(track_msg.left)
-        self.right = int(track_msg.right)
-        rospy.loginfo("Get track info: {:d}, {:d}".format(self.left, self.right))
+        if key == ord('q'):
+            self.terminate = True
 
 if __name__ == "__main__":
     cv2.namedWindow("Visualizer")
